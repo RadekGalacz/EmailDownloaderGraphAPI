0baf476c7aba081a77f128b8a14981b2087bda30
RadekGalacz <rgalacz@gmail.com>
2025-07-26 09:59:51 +0200

    Refrakotirzace - rozdělení kódu v hlavní metodě dowloadEmails na menší celky, přidání asynhronního zápisu a načítání Idček do souboru, konkretnéjší kometáře v kódu

--- EmailDownloaderGraphAPI/Classes/EmailDownloader.cs
+++ EmailDownloaderGraphAPI/Classes/EmailDownloader.cs
@@ -7,7 +7,7 @@ namespace EmailGraphAPI.Classes {
         private AppConfigProps config = new AppConfigProps();
         private static readonly ILog log = LogManager.GetLogger(typeof(Program)); // Inicializace loggeru pro třídu Program (log4net)
         private readonly GraphAuthProvider graphAuthProvider;
-        private GraphServiceClient graphClient; // Graph API
+        private GraphServiceClient graphClient; // Graph API pro poskytování metod pro práci s emaily
 
         // Konstruktor třídy EmailDownloader
         public EmailDownloader( GraphAuthProvider graphAuthProvide, AppConfigProps config) {
@@ -15,40 +15,126 @@ namespace EmailGraphAPI.Classes {
             this.config = config;
         }
 
-        // HLAVNÍ METODA pro stahování emailů
-        //___________________________________
+        // Metoda pro kontrolu autentizace emailů z config.JSON
+        public void AuthenticationMailBoxesCheck() {
 
+            if (!config.AllowedMailBoxes.Contains(config.Mailbox)) {
+                log.Error($"Nepovolený pokus o přístup ke schránce: {config.Mailbox}");
+
+                throw new UnauthorizedAccessException($"Nepovolený přístup k {config.Mailbox}");
+            }
+            else {
+                log.Info($"Autorizovaný přístup ke schránce: {config.Mailbox}");
+            }
+        }
+
+        // Metoda vrací seřazené emaily v pořadí, v jakém přišly
+        public async Task<List<Microsoft.Graph.Models.Message>> LoadEmailsAsync() {
+
+            log.Info("=== Spouštím aplikaci pro stahování e-mailů ===");
+
+            // Inicializace Graph API klienta (pokud ještě není inicializován)
+            if (graphClient == null) {
+                graphClient = graphAuthProvider.GetAuthenticatedClient();
+            }
+
+            // Načtení seznamu zpráv z inboxu zadané emailové schránky
+            var messages = await graphClient.Users[config.Mailbox]
+                .MailFolders["Inbox"]
+                .Messages
+                .GetAsync();
+
+            // Seřazení zpráv podle data přijetí
+            var orderedMessages = messages.Value.OrderBy(msg => msg.ReceivedDateTime).ToList();
+            return orderedMessages;
+        }
+
+        // Metoda pro vytvoření složky (pokud neexistuje) dle cesty načtené z config.JSON
+        public void CreateFolderForEmails() {
+            DirectoryInfo di = new DirectoryInfo(config.DownloadPath);
+            int i = 1;
+
+            // Kontrola, zda složka už existuje
+            if (di.Exists) {
+                DirectoryInfo[] subfolders = di.GetDirectories();
+
+                string vypis = subfolders.Length == 0 ? "---" : string.Join("\n", subfolders.Select(d => $"[{i++}] {d.Name}")); // Vypíše názvy již stažených podsložek dle předmětů emailů
+
+                log.Info($"Složka pro ukládání emailu úspěšně načtená {config.DownloadPath}, aktuálně obsahuje tyto podsložky:\n{vypis}");
+            }
+            else {
+                // Pokud složka neexistuje, votvořit ji
+                log.Info($"Vytvářím novou složku pro e-maily: {config.DownloadPath}");
+                di.Create();
+            }
+        }
+
+        // Asynchronní metoda pro ukládání id emailů do souboru donwloadedEmails.TXT
+        public async Task SaveIdsToFile(string id) {
+            if (!string.IsNullOrWhiteSpace(id)) {
+                // Uložení ID emailo do souboru downloadedEmails.TXT pro zamezení opětovného stažení
+                string downloadedPath = Path.Combine(config.DownloadPath, "downloadedEmails.TXT");
+                await File.AppendAllTextAsync(downloadedPath, id + Environment.NewLine);
+                log.Debug($"Zapsáno ID emailu do seznamu stažených: {id}");
+            }
+        }
+
+        // Asynchronní metoda pro načitání id emailů ze souboru donwloadedEmails.TXT
+        public async Task<List<string>> GetSavedIds() {
+            string downloadedPath = Path.Combine(config.DownloadPath, "downloadedEmails.TXT");
+
+            if (!File.Exists(downloadedPath)) {
+                log.Warn("Soubor downloadedEmails.TXT neexistuje – bude vytvořen při prvním stažení.");
+                return new List<string>();  // Soubor neexistuje, přidat pouze prázdný seznam
+            }
+            try {
+                string[] lines = File.ReadAllLines(downloadedPath);
+                var ids = new List<string>();
+
+                // Přidání ID emailů načtených z .TXT souboru
+                foreach (var line in lines) {
+                    if (!string.IsNullOrWhiteSpace(line)) {
+                        ids.Add(line.Trim());  // Přidat řádky
+                    }
+                }
+                return ids;
+            }
+            catch (IOException ex) {
+                log.Error($"Chyba při čtení souboru downloadedEmails.TXT: {ex.Message}", ex);
+                throw;
+            }
+        }
+
+        // Metoda pro vytvoření unikátních názvu podsložek
+        public async Task <string> CreateUniqueFolderPath(string basePath, string subject) {
+            // Oříznutí délky názvu předmětu na maxLength
+            var maxLength = 100;
+            if (subject.Length > maxLength) {
+                subject = subject.Substring(0, maxLength);
+            }
+            // Vytvoření podsložek s ochranou proti zakázaným znakům
+            var subjectName = string.Join("_", subject.Split(Path.GetInvalidFileNameChars()));
+            var subfolderPath = Path.Combine(basePath, subjectName);
+
+            // Pokud složka existuje, přidat  číslování _1 atd.
+            int i = 1;
+            while (Directory.Exists(subfolderPath)) {
+                subfolderPath = Path.Combine(config.DownloadPath, subjectName + "_" + i);
+                i++;
+            }
+            return subfolderPath;
+        }
+
+        // HLAVNÍ METODA pro ukládání emailů do složek
         public async Task DownloadEmails() {
 
             try {
-                log.Info("=== Spouštím aplikaci pro stahování e-mailů ===");
-
-                // Kontrola autentizovanách emailových schránek
+                // Ověření přístupu podle "AllowedMailBoxes" z config .json
                 AuthenticationMailBoxesCheck();
-
-                // Inicializace Graph API klienta (pokud ještě není inicializován)
-                if (graphClient == null) {
-                    graphClient = graphAuthProvider.GetAuthenticatedClient();
-                }
-
-                // Načtení seznamu zpráv z inboxu zadané emailové schránky
-
-                var messages = await graphClient.Users[config.Mailbox]
-                    .MailFolders["Inbox"]
-                    .Messages
-                    .GetAsync();
-
-                // Seřazení zpráv podle data přijetí
-                var orderedMessages = messages.Value.OrderBy(msg => msg.ReceivedDateTime).ToList();
-
-                // Vytvoření složky (pokud neexistuje) dle cesty načtené z config.JSON
-                CreateFolderForEmails();
-
-                // Načtení uložených Idček emailů z donwloadedEmails.TXT
-                List<string> downloadedIds = GetDownloadedIds();
 
                 // Parsoání datumu z config.JSON
                 var configStartDate = DateTime.ParseExact(config.StartDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                var orderedMessages = await LoadEmailsAsync(); // Přiřazení metody načtených emaliů do proměnné
 
                 // Cysklus pro každý email ve složce Inbox
                 foreach (var msg in orderedMessages) {
@@ -69,11 +155,11 @@ namespace EmailGraphAPI.Classes {
                         continue;
                     }
 
-                    // Metoda pro vytvoření unikátních složek
-                    var subfolderPath = CreateUniqueFolderPath(config.DownloadPath, msg.Subject);  // Přiřazení výstupu do proměnné subfolderPath
+                    var subfolderPath = await CreateUniqueFolderPath(config.DownloadPath, msg.Subject);  // Přiřazení metody pro vytvoření unikátních názvů složek do proměnné
+                    var savedIDs = await GetSavedIds(); // Přiřazení metody pro načtení ID uložených z .TXT do proměnné
 
                     // Pokud podsložky existují, přerušit stahování
-                    if (downloadedIds.Contains(msg.InternetMessageId)) {
+                    if (savedIDs.Contains(msg.InternetMessageId)) {
                         log.Info($"Přeskakuji – email už byl dříve stažen (ID: {msg.InternetMessageId})");
                         continue;
                     }
@@ -87,103 +173,20 @@ namespace EmailGraphAPI.Classes {
                             log.Error($"Chyba při vytváření složky {subfolderPath}: {ex.Message}", ex);
                         }
 
-                        // Uložení emailů do souboru
+                        // Uložení emailů do souboru ve formátu .eml
                         var filePath = Path.Combine(subfolderPath, $"message.eml");
                         using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
                         await content.CopyToAsync(fs);
 
                         log.Info($"E-mail úspěšně uložen: {filePath}");
 
-                        // Přidat ID emailu do seznamu stažených emailů, aby se nestahoval znovu.
-                        DownloadedIds(msg.InternetMessageId);
+                        // Přidání ID emailu do seznamu stažených emailů, aby se nestahoval znovu.
+                        await SaveIdsToFile(msg.InternetMessageId);
                     }
                 }
             }
             catch (Exception ex) {
                 log.Fatal("Chyba při stahování e-mailů", ex);
-            }
-        }
-
-        // POMOCNÉ METODY
-        //___________________________________
-
-        // Pomocná metoda pro vytvoření složky (pokud neexistuje) dle cesty načtené z config.JSON
-        public void CreateFolderForEmails() {
-            DirectoryInfo di = new DirectoryInfo(config.DownloadPath);
-            int i = 1;
-
-            if (di.Exists) {
-                DirectoryInfo[] subfolders = di.GetDirectories();
-
-                string vypis = subfolders.Length == 0 ? "---" : string.Join("\n", subfolders.Select(d => $"[{i++}] {d.Name}")); // Vypíše názvy již stažených podsložek dle předmětů emailů
-
-                log.Info($"Složka pro ukládání emailu úspěšně načtená {config.DownloadPath}, aktuálně obsahuje tyto podsložky:\n{vypis}");
-            }
-            else {
-                log.Info($"Vytvářím novou složku pro e-maily: {config.DownloadPath}");
-                di.Create();
-            }
-        }
-
-        // Pomocná metoda pro vytvoření unikátních podsložek
-        public string CreateUniqueFolderPath(string basePath, string subject) {
-            // Vytvoření podsložek s ochranou proti zakázaným znakům
-            var subjectName = string.Join("_", subject.Split(Path.GetInvalidFileNameChars()));
-            var subfolderPath = Path.Combine(basePath, subjectName);
-
-            // Pokud složka existuje, přidat  číslování _1 atd.
-            int i = 1;
-            while (Directory.Exists(subfolderPath)) {
-                subfolderPath = Path.Combine(config.DownloadPath, subjectName + "_" + i);
-                i++;
-            }
-            return subfolderPath;
-        }
-
-        // Pomocná metoda pro ukládání id emailů do souboru donwloadedEmails.TXT
-        public void DownloadedIds(string id) {
-            if (!string.IsNullOrWhiteSpace(id)) {
-                string downloadedPath = Path.Combine(config.DownloadPath, "downloadedEmails.TXT");
-                File.AppendAllText(downloadedPath, id + Environment.NewLine);
-                log.Debug($"Zapsáno ID emailu do seznamu stažených: {id}");
-            }
-        }
-
-        // Pomocná metoda pro načitání id emailů ze souboru donwloadedEmails.TXT
-        public List<string> GetDownloadedIds() {
-            string downloadedPath = Path.Combine(config.DownloadPath, "downloadedEmails.TXT");
-
-            if (!File.Exists(downloadedPath)) {
-                log.Warn("Soubor downloadedEmails.TXT neexistuje – bude vytvořen při prvním stažení.");
-                return new List<string>();  // Soubor neexistuje, přidat pouze prázdný seznam
-            }
-            try {
-                string[] lines = File.ReadAllLines(downloadedPath);
-                var ids = new List<string>();
-
-                foreach (var line in lines) {
-                    if (!string.IsNullOrWhiteSpace(line)) {
-                        ids.Add(line.Trim());  // Přidat řádky
-                    }
-                }
-                return ids;
-            }
-            catch (IOException ex) {
-                log.Error($"Chyba při čtení souboru downloadedEmails.TXT: {ex.Message}", ex);
-                throw;
-            }
-        }
-
-        // Pomocná metoda pro kontrolu autentizace emailů z config.JSON
-        public void AuthenticationMailBoxesCheck() {
-
-            if (!config.AllowedMailBoxes.Contains(config.Mailbox)) {
-                log.Error($"Nepovolený pokus o přístup ke schránce: {config.Mailbox}");
-
-                throw new UnauthorizedAccessException($"Nepovolený přístup k {config.Mailbox}");
-            }
-            else {
-                log.Info($"Autorizovaný přístup ke schránce: {config.Mailbox}");
             }
         }
     }
